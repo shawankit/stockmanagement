a4d44faa34a2a4ad0dc3326143046b3287023e59
Ankit Shaw <shawankit80@gmail.com>
2022-05-30 21:10:19 +0530

    added supplier count

--- resources/stats/queries/get-all-stats-query.js
+++ resources/stats/queries/get-all-stats-query.js
@@ -1,4 +1,4 @@
-const { Item, Consignment, Godown, sequelize } = require("../../../models");
+const { Item, Consignment, Godown, sequelize, Supplier } = require("../../../models");
 
 module.exports = class GetAllStatsQuery {
     constructor(){
@@ -11,11 +11,13 @@ module.exports = class GetAllStatsQuery {
          as conCountfrom from consignments group by "consignmentNo") as t1`);
          const [[{packageCount}]] = await sequelize.query(`SELECT SUM((CASE WHEN "numberOfPackage"~E'^\\\\d+$' THEN "numberOfPackage"::integer ELSE 0 end)) as "packageCount" FROM consignments`);
         const godownCount = await Godown.count();
+        const supplierCount = await Supplier.count();
         return {
             itemCount,
             consignmentCount,
             packageCount,
-            godownCount
+            godownCount,
+            supplierCount
         }
     }
 }
