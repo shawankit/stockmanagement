29d6fa052056cce9af1eb80566b01edc1a1e89c4
Ankit Shaw <shawankit80@gmail.com>
2022-06-02 23:05:03 +0530

    Added Search Faeture

--- resources/consigments/queries/get-all-consigments-query.js
+++ resources/consigments/queries/get-all-consigments-query.js
@@ -1,25 +1,91 @@
 const { Consignment, Godown, Sequelize } = require("./../../../models");
 const R = require('ramda');
 module.exports = class GetAllConsignmentsQuery {
-    constructor(consignmentNo, offset = 0, limit = 10){
-        this.consignmentNo = consignmentNo
+    constructor(pattern, offset = 0, limit = 10){
+        this.pattern = pattern
         this.limit = limit;
         this.offset = offset;
     }
 
     get(){
       
-        if(!R.isNil(this.consignmentNo)){
-            console.log(this.consignmentNo);
+        if(!R.isNil(this.pattern)){
             return Consignment.findAll({
                 where: {
-                    consignmentNo: {
-                        [Sequelize.Op.iLike]:  `${this.consignmentNo}%`
-                    }
+                    [Sequelize.Op.or]: [
+                        {
+                            consignmentNo: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            transporter: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            supplier: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            item: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            billNo: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            mrno: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            challanNumber: {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            }
+                        },
+                        {
+                            '$godowns.name$': {
+                                [Sequelize.Op.iLike]:  `${this.pattern}%`
+                            },
+                            '$godowns."ConsigmentLocation"."createdAt"$': {
+                                [Sequelize.Op.eq]: Sequelize.literal(`(
+                                        select
+                                            max(cl."createdAt")
+                                        from
+                                            "consigmentLocations" as cl
+                                        where
+                                            cl."consignmentId" = "Consignment".id )`)
+                            }
+                        },
+                    ]
                 },
                 include : [{
                     model: Godown,
-                    as: 'godowns'
+                    as: 'godowns',
+                    order: [
+                        ['updatedAt', 'DESC']
+                    ],
+                    // where: {
+                    //     id: {
+                    //         [Sequelize.Op.eq]: Sequelize.literal(`(select
+                    //     clc."godownId" as id
+                    // from
+                    //     "consigmentLocations" as clc
+                    // where
+                    //     clc."createdAt" = (
+                    //     select
+                    //         max(cl."createdAt")
+                    //     from
+                    //         "consigmentLocations" as cl
+                    //     where
+                    //         cl."consignmentId" = "Consignment".id ))`)
+                    //     }
+                    // }
                 }],
                 order: [
                     ['updatedAt', 'DESC']
@@ -30,7 +96,10 @@ module.exports = class GetAllConsignmentsQuery {
         return Consignment.findAll({
             include : [{
                 model: Godown,
-                as: 'godowns'
+                as: 'godowns',
+                order: [
+                    ['updatedAt', 'DESC']
+                ],
             }],
             order: [
                 ['updatedAt', 'DESC']
